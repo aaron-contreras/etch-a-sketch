01064c502dc0bfe8d5dd3f465abbb66808c768f4
Aaron Contreras <aaronlcaq@gmail.com>
2020-04-23 21:15:47 -0500

    Add initial styling and color palette

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const gridContainer = document.getElementById('grid-container');
+const gridContainerBackground = getComputedStyle(document.documentElement).getPropertyValue('--container-background');
+console.log(gridContainerBackground);
 const defaultDimensions = 16;
 let colorMode = 'single-color';
 let paintMode = 'standard';
@@ -8,11 +10,30 @@ const colorList = [
   '0,0,255', '128,0,128', '255,192,203',
   '128,128,128'
 ];
+const toggleGridButton = document.getElementById('toggle-grid');
+toggleGridButton.addEventListener('click', () => {
+  cells.forEach(cell => cell.classList.toggle('show-hide-grid'));
+});
 const colorPaletteContainer = document.getElementById('palette');
+const clickableColors = colorPaletteContainer.children;
 let paintingColor = colorList[0];
+const erase = document.getElementById('erase');
+
+function enableErasing(event) {
+  paintingColor = gridContainerBackground;
+  console.log(paintingColor);
+}
+erase.addEventListener('click', enableErasing);
 
 function setColor(colorClicked) {
   paintingColor = colorClicked.target.getAttribute('data-color');
+  [...clickableColors].forEach(color => {
+    if (color == colorClicked.target) {
+      color.classList.add('active-color');
+    } else {
+      color.classList.remove('active-color');
+    }
+  });
 }
 
 function createColorPalette() {
@@ -66,6 +87,7 @@ function setPrecisionModeIfActive() {
 function toggleModes(event) {
   if ([...event.target.classList].includes('painting')) {
     paintMode = event.target.id;
+    console.log(paintMode);
     setPrecisionModeIfActive();
     paintingModeButtons.forEach(button => {
       if (button == event.target) {
@@ -76,6 +98,7 @@ function toggleModes(event) {
     });
   } else {
     colorMode = event.target.id;
+    console.log(colorMode);
     colorModeButtons.forEach(button => {
       if (button == event.target) {
         button.classList.add('active-mode');
@@ -95,6 +118,10 @@ paintingModeButtons.forEach(button => {
   button.addEventListener('click', toggleModes);
 });
 
+colorModeButtons[0].classList.add('active-mode');
+paintingModeButtons[0].classList.add('active-mode');
+
+
 /*OPACITY MODE FUNCTION*/
 
 function increaseOpacity(hoveredCell) {
@@ -107,15 +134,21 @@ function increaseOpacity(hoveredCell) {
 }
 
 function fillCell(hoveredCell) {
+  console.log(paintingColor);
     if (colorMode == 'single-color') {
-      hoveredCell.target.style.backgroundColor = `rgb(${paintingColor}`;
+      hoveredCell.target.style.backgroundColor = `rgb(${paintingColor})`;
+
     } else if (colorMode == 'picasso') {
-      hoveredCell.target.style.backgroundColor = getRandomColor();
+      if (paintingColor == gridContainerBackground) {
+        hoveredCell.target.style.backgroundColor = '';
+      } else {
+        hoveredCell.target.style.backgroundColor = getRandomColor();
+      }
     } else {
       increaseOpacity(hoveredCell);
     }
 }
-
+let cells = '';
 function createGrid(dimensions) {
   for (let i = 0; i < dimensions; i++) {
     const cell = document.createElement('div');
@@ -124,10 +157,11 @@ function createGrid(dimensions) {
     cell.addEventListener('mouseover', fillCell);
     gridContainer.appendChild(cell);
   }
+  cells = [...gridContainer.children];
 }
 
 createGrid(defaultDimensions ** 2);
-const cells = [...gridContainer.children];
+
 const clearGridButton = document.getElementById('clear');
 
 function askNewGridSize() {
@@ -152,8 +186,8 @@ function resizeGrid() {
 }
 
 function clearGrid() {
-  cells.forEach(cell => cell.classList.remove('hovered'));
   resizeGrid();
+  setPrecisionModeIfActive();
 }
 
 clearGridButton.addEventListener('click', clearGrid);
